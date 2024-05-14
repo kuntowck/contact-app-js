05e46a2fc3c8ebe01896343f50f9be86b179fbc3
kuntowck <kunto312002@gmail.com>
2024-05-14 09:17:14 +0700

    halaman contact

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+require("./utils/database");
+const Contact = require("./model/contact");
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs"); // setup ejs
 app.use(expressLayouts); // setup express-layouts
+
+// config flash
+app.use(cookieParser("secret")); 
+app.use(
+  session({
+    cookie: { maxAge: 6000 },
+    secret: "secret",
+    resave: "true",
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
 app.use(express.static("public")); // allow file static
 app.use(express.urlencoded({ extended: true })); // parse data URL
 
@@ -25,6 +43,18 @@ app.get("/about", (req, res) => {
   res.render("about", {
     title: "About",
     layout: "layouts/main",
+  });
+});
+
+// halaman contact
+app.get("/contact", async (req, res) => {
+  const contacts = await Contact.find();
+
+  res.render("contact", {
+    title: "Contact",
+    layout: "layouts/main",
+    contacts,
+    msg: req.flash("msg"),
   });
 });
 
