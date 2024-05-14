c216962b2330bc4602f3d3c3b9ba4fd2a834286a
kuntowck <kunto312002@gmail.com>
2024-05-14 14:45:06 +0700

    fitur delete data contact dengan method delete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const { body, validationResult, check } = require("express-validator");
+const methodOverride = require("method-override");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
@@ -9,6 +10,8 @@ const Contact = require("./model/contact");
 
 const app = express();
 const port = 3000;
+
+app.use(methodOverride("_method")); // setup override
 
 app.set("view engine", "ejs"); // setup ejs
 app.use(expressLayouts); // setup express-layouts
@@ -100,18 +103,11 @@ app.post(
 );
 
 // delete data contact process
-app.get("/contact/delete/:name", async (req, res) => {
-  const contact = await Contact.findOne({ nama: req.params.name });
-
-  if (!contact) {
-    res.status(404);
-    res.send("<h1>404</h1>");
-  } else {
-    Contact.deleteOne({ _id: contact._id }).then((result) => {
-      req.flash("msg", "Data contact berhasil dihapus!");
-      res.redirect("/contact");
-    });
-  }
+app.delete("/contact", (req, res) => {
+  Contact.deleteOne({ nama: req.body.nama }).then((result) => {
+    req.flash("msg", "Data contact berhasil dihapus!");
+    res.redirect("/contact");
+  });
 });
 
 // halaman detail contact
